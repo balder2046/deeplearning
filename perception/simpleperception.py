201e3a4de619e3cc5830dd3882aa5dd3829de24f
zhaolei <zhaolei@objectgames.com>
2018-08-18 17:15:37 +0800

    simple perception algorithm

--- perception/simpleperception.py
+++ perception/simpleperception.py
@@ -60,8 +60,100 @@ def show_some_mnist_images(n,x_train,y_train):
         cv2.imshow('image',M)
         c = cv2.waitKey(0)
         cv2.destroyAllWindows()
+
+def test_weights(weights,x_test,y_test):
+    correct_rate,nr_correct,nr_wrong = test_perceptron(weights,x_test,y_test)
+    print(correct_rate * 100.0," % of the test patterns", " were correcty classified.")
+    print("correctly classified: ",nr_correct)
+    print("wrongly classified:" ,nr_wrong)
+    print("*************************************************")
+
+
+
+'''
+Generate a weight matrix of dimension
+(nr-of-inputs, nr-of-outputs)
+and train the weights according to the Perception learning rule use random sample 
+patterns <input,desired output> from the MNIST training dataset
+'''
+def generate_and_train_perception_classifier(nr_train_steps,x_train,y_train,x_test,y_test):
+    nr_train_examples = x_train.shape[0]
+    # 1. generate Perceptron with random weights
+    weights = np.random.rand(785,10)
+    # 2. do the desired number of trainning steps
+    for train_step in range(0,nr_train_steps):
+        if train_step % 1000 == 0 and train_step > 0:
+            print("train step ", train_step)
+            test_weights(weights,x_test,y_test)
+        # 2.2 choose a random image
+        rnd_number = randint(0,nr_train_examples - 1)
+        
+        input_vec = x_train[rnd_number,:]
+        # add bias input "1"
+        input_vec = np.append(input_vec,[1])
+        input_vec = input_vec.reshape(1,785)
+        # 2.3 compute Percetron output
+        act = np.matmul(input_vec,weights)
+        out_mat = f(act)
+        # 2.4 compute difference vector
+        teacher_out_mat = y_train[rnd_number,:]
+        teacher_out_mat = teacher_out_mat.reshape(1,10)
+        diff_mat = teacher_out_mat - out_mat
+        #2.5 correct weights
+        learn_rate = 0.01
+        for neuron_nr in range(0,10):
+            # 2.5.1 get neuron error
+            neuron_error = diff_mat[0,neuron_nr]
+            # 2.5.2 for all weights to the current
+            # neuron
+            for weight_nr in range(0,785):
+                x_i = input_vec[0,weight_nr]
+                delta_w_i = learn_rate * neuron_error * x_i
+                weights[weight_nr,neuron_nr] += delta_w_i
+    return weights
+
+'''
+New test how good the Perceptron can classify on data never seen before
+i.e., the test  data
+'''
+def test_perceptron(weights,x_test,y_test):
+    nr_test_examples = x_test.shape[0]
+
+    # 1. initialize counters
+    nr_correct = 0
+    nr_wrong = 0
+    # 2. forward all test patterns
+    # then compare predicted label with ground truth label and check wheter
+    # the prediction is right or not
+    for test_vec_nr in range(0,nr_test_examples):
+        input_vec = x_test[test_vec_nr,:]
+        input_vec = np.append(input_vec,[1])
+        input_vec = input_vec.reshape(1,785)
+        # 2.2 get the desired output vector
+        teacher_out_mat = y_test[test_vec_nr,:]
+        teacher_out_mat = teacher_out_mat.reshape(1,10)
+        teacher_class = np.argmax(teacher_out_mat)
+        act = np.matmul(input_vec,weights)
+        out_mat = f(act)
+        actual_class = np.argmax(out_mat)
+        if teacher_class == actual_class:
+            nr_correct += 1
+        else:
+            nr_wrong += 1
+    correct_rate = float(nr_correct) / float(nr_correct + nr_wrong)
+    return correct_rate,nr_correct,nr_wrong
+
+
+
+
 def main():
     x_train,y_train,x_test,y_test = read_mnist_data()
-    show_some_mnist_images(10,x_train,y_train)
+    #show_some_mnist_images(10,x_train,y_train)
+    weights = generate_and_train_perception_classifier(100000,x_train,y_train,x_test,y_test)
+    correct_rate,nr_correct,nr_wrong = test_perceptron(weights,x_test,y_test)
+    print(correct_rate * 100.0," % of the test patterns", " were correcty classified.")
+    print("correctly classified: ",nr_correct)
+    print("wrongly classified:" ,nr_wrong)
+    print("Program end.")
 if __name__ == "__main__":
     main()
